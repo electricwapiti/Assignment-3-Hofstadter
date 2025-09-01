c6ccad15bdacd13a9b1f42ffe9a712b398daa11c
electricwapiti <113694725+electricwapiti@users.noreply.github.com>
2025-08-31 20:49:54 -0500

    Initial commit

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+      Hofstadter hofstadter = new Hofstadter();
+      System.out.println(hofstadter.gSequence(5));
+    }
+}
